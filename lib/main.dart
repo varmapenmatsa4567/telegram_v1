69cc571fede0c9c75fabcd467922634dd244c161
varmapenmatsa4567 <varmapenmatsa4567@gmail.com>
2023-09-18 18:39:23 +0530

    Firebase Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import "package:get/get.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase Initialization
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Run the app
   runApp(const MyApp());
 }
 
